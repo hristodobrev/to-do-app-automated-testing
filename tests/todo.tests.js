089ff7e73ea41a436c9c1702050c28d40d4ab72b
Hristo Dobrev <hristo.nankov1@gmail.com>
2023-11-13 23:51:57 +0200

    Added tests

---
+++ tests/todo.tests.js
@@ -0,0 +1,50 @@
+const { test, expect } = require('@playwright/test');
+
+const baseUrl = 'http://localhost:5500/';
+
+test('user can add a task', async ({ page }) => {
+  await page.goto(baseUrl);
+  await page.fill('#task-input', 'Test Task');
+  await page.click('#add-task');
+
+  const taskText = await page.textContent('.task');
+
+  expect(taskText).toContain('Test Task');
+});
+
+test('user can delete a task', async ({ page }) => {
+  await page.goto(baseUrl);
+  await page.fill('#task-input', 'Test Task');
+  await page.click('#add-task');
+
+  await page.click('.task .delete-task');
+
+  const tasks = await page.$$eval('.task', tasks => tasks.map(task => task.textContent));
+
+  expect(tasks).not.toContain('Test Task');
+});
+
+test('user can mark a task as complete', async ({ page }) => {
+  await page.goto(baseUrl);
+  await page.fill('#task-input', 'Test Task');
+  await page.click('#add-task');
+
+  await page.click('.task .task-complete');
+
+  const completedTask = await page.$('.task.completed');
+
+  expect(completedTask).not.toBeNull();
+});
+
+test('user can filter tasks', async ({ page }) => {
+  await page.goto(baseUrl);
+  await page.fill('#task-input', 'Test Task');
+  await page.click('#add-task');
+
+  await page.click('.task .task-complete');
+
+  await page.selectOption('#filter', 'Completed');
+
+  const incompleteTask = await page.$('.task:not(.completed)');
+  expect(incompleteTask).toBeNull();
+});
